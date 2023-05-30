9ab9b6efb83096d64bcdd72f61d493b8742bc0bc
mesciuS <giovadev@gmail.com>
2023-05-30 15:26:36 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,41 @@
+<script>
+  import axios from "axios";
+  import ProjectCard from './ProjectCard.vue';
+
+  export default {
+    name: 'AppMain',
+    data() {
+      return {
+        projects: []
+      }
+    },
+    components: {
+      ProjectCard,
+    },
+    mounted() {
+      this.getProjects();
+    },
+    methods: {
+      getProjects() {
+        axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+          console.log(response.data.results);
+          this.projects = response.data.results;
+        });
+      }
+    },
+  }
+</script>
+
+<template>
+  <div class="container pt-3">
+    <div class="row">
+      <div v-for="project in projects" class="col-4 pt-3">
+        <ProjectCard :project="project"></ProjectCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        project: Object
+    }
+}
+</script>
+<template>
+    <div class="project">
+      <strong> - {{ project.title }}</strong>
+      <p>{{ project.text }}</p>
+    </div>
+</template>
+<style lang="scss" scoped>
+.project {
+    border: 1px solid brown;
+    border-radius: 5px;
+    padding: 10px;
+}
+</style>
